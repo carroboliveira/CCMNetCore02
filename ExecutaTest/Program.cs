fe1fb49df3c92acf7dae335c5803abfcdc80d004
carlos.oliveira <carlos.oliveira@starline.inf.br>
2020-02-13 11:03:16 -0200

    implementacao para Receber Parametros

---
+++ ExecutaTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluxoVendaCartoes;
+
+namespace ExecutaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VendaCartoes venda = new VendaCartoes("Chrome");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
